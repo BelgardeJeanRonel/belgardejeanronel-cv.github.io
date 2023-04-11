4b53fa617945f55bf768a54e705796b1bcbc6746
Belgarde Jean Ronel <belganel@gmail.com>
2023-04-11 17:32:45 +0200

    modification des icones réseaux

--- js/app.js
+++ js/app.js
@@ -68,3 +68,25 @@ linkNavMenu.forEach(a => {
         })
     })
 })
+
+const ratio = .1;
+const options = {
+    root : null,
+    rootMargin : "0px",
+    threshold : ratio
+}
+
+const handleIntersect = function (entries, observer) {
+    entries.forEach( entry => {
+        if (entry.intersectionRatio > ratio){
+            entry.target.classList.remove("reveal");
+            observer.unobserve(entry.target);
+        }
+    })
+}
+
+document.documentElement.classList.add("reveal-loaded");
+const observer = new IntersectionObserver(handleIntersect, options);
+document.querySelectorAll('.reveal').forEach( r => {
+    observer.observe(r)
+})
